22240d62cecc7982047923d33c4cf2fde865805e
yguerch212-creator <yguerch212@gmail.com>
2025-09-25 09:32:59 +0200

    Update swb_spawnmenu.lua

--- addons/strategic_war_board/lua/autorun/swb_spawnmenu.lua
+++ addons/strategic_war_board/lua/autorun/swb_spawnmenu.lua
@@ -1,1 +1,5 @@
-
+-- Permet de spawn l'entité depuis le spawnmenu (Sandbox)
+-- Dans DarkRP, vous l’exposerez via F4 plus tard (catégorie/entité).
+if CLIENT then
+    language.Add("ent_swb_board", "Strategic War Board (proto)")
+end
