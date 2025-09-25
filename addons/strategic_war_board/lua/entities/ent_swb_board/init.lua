f28effe405b6558c63dae4b2841dca17d79a80a2
yguerch212-creator <yguerch212@gmail.com>
2025-09-25 09:32:07 +0200

    Update init.lua

--- addons/strategic_war_board/lua/entities/ent_swb_board/init.lua
+++ addons/strategic_war_board/lua/entities/ent_swb_board/init.lua
@@ -1,1 +1,19 @@
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("cl_init.lua")
+include("shared.lua")
 
+function ENT:Initialize()
+    self:SetModel("models/props_c17/frame002a.mdl") -- cadre/tableau simple
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+    local phys = self:GetPhysicsObject()
+    if IsValid(phys) then phys:Wake() end
+end
+
+-- Utilisation: ouvre l’UI côté client (proto)
+function ENT:Use(activator, caller)
+    if not IsValid(activator) or not activator:IsPlayer() then return end
+    net.Start("swb_open_ui")
+    net.Send(activator)
+end
