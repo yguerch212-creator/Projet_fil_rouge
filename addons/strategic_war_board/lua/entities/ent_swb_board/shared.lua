dcc1e8352c62085a00d029e8e6678a7021b8c8bd
yguerch212-creator <yguerch212@gmail.com>
2025-09-25 09:31:50 +0200

    Update shared.lua

--- addons/strategic_war_board/lua/entities/ent_swb_board/shared.lua
+++ addons/strategic_war_board/lua/entities/ent_swb_board/shared.lua
@@ -1,1 +1,11 @@
+ENT.Type      = "anim"
+ENT.Base      = "base_gmodentity"
+ENT.PrintName = "Strategic War Board (proto)"
+ENT.Author    = "Yanis Guerch"
+ENT.Spawnable = true
+ENT.Category  = "Strategic War Board"
 
+-- Réseau : identifiant du net channel pour ouvrir l’UI
+if SERVER then
+    util.AddNetworkString("swb_open_ui")
+end
