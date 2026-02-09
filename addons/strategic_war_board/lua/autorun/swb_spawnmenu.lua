701d380dac94b9820e3e3f6c0f0c62fd6f667fc1
yguerch212-creator <yguerch212@gmail.com>
2026-02-09 14:31:47 +0100

    Delete addons/strategic_war_board directory

--- addons/strategic_war_board/lua/autorun/swb_spawnmenu.lua
+++
@@ -1,5 +0,0 @@
--- Permet de spawn l'entité depuis le spawnmenu (Sandbox)
--- Dans DarkRP, vous l’exposerez via F4 plus tard (catégorie/entité).
-if CLIENT then
-    language.Add("ent_swb_board", "Strategic War Board (proto)")
-end
